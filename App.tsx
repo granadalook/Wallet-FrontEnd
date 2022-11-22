b4df8c44583527f96203cb3bed46e8e00c7b4af2
granadalook <tiven17.jsga@gmail.com>
2022-11-21 20:04:46 -0500

    ADD/Se agrega react navigation para  NavBar  de la aplicacion

--- App.tsx
+++ App.tsx
@@ -1,42 +1,9 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
-import {Contraseña} from './src/app/screens/contraseña';
-import {Home} from './src/app/screens/home';
-import {Login} from './src/app/screens/login';
+
 import {NavBar} from './src/app/screens/navBar';
-import {Pagos} from './src/app/screens/pagos';
-import {Prestamos} from './src/app/screens/prestamos';
-import {Registro} from './src/app/screens/registro';
 
 const App = () => {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text>Container Principal</Text>
-      </View>
-      <View>
-        <NavBar />
-      </View>
-      <View>
-        <Home />
-      </View>
-      <View>
-        <Login />
-      </View>
-      <View>
-        <Prestamos />
-      </View>
-      <View>
-        <Registro />
-      </View>
-      <View>
-        <Pagos />
-      </View>
-      <View>
-        <Contraseña />
-      </View>
-    </SafeAreaView>
-  );
+  return <NavBar />;
 };
 
 export default App;
--- src/app/screens/navBar.tsx
+++ src/app/screens/navBar.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import {Registro} from './registro';
+import {Pagos} from './pagos';
+import {Prestamos} from './prestamos';
+import {NavigationContainer} from '@react-navigation/native';
+import {Login} from './login';
+import {Home} from './home';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="REGISTRO" component={Registro} />
+      <Tab.Screen name="PAGOS" component={Pagos} />
+      <Tab.Screen name="PRESTAMOS" component={Prestamos} />
+      <Tab.Screen name="login" component={Login} />
+      <Tab.Screen name="HOME" component={Home} />
+    </Tab.Navigator>
+  );
+}
+
 export const NavBar = () => {
   return (
-    <View>
-      <Text>Barra de navegacion</Text>
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 };
